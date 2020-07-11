bef534ddc05a90b3f678d7db17cfc628a9d1357b
Andrei Voicu <dreivoicu@gmail.com>
2020-07-11 12:43:25 +0300

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppLoadModule } from './app.load.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 import { FeatureFlagDirective } from './feature-flag.directive';
 
 @NgModule({
-  declarations: [AppComponent, FeatureFlagDirective],
-  imports: [AppLoadModule, BrowserModule],
+  declarations: [AppComponent, HomeComponent, FeatureFlagDirective],
+  imports: [AppLoadModule, AppRoutingModule, BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
